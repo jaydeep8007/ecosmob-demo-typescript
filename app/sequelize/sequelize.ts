918e80fc6fec4876d1512c1e0e5d14c52566b415
Jaydeep Parmar <parjaydeep018@gmail.com>
2025-05-28 17:35:45 +0530

    add winston logger in sql

--- app/sequelize/sequelize.ts
+++ app/sequelize/sequelize.ts
@@ -1,4 +1,6 @@
+// app/sequelize/sequelize.ts
 import { Sequelize } from 'sequelize';
+import logger from '../utils/utils.logger';
 
 const sequelize = new Sequelize(
   process.env.MYSQL_DB_NAME as string,
@@ -8,8 +10,17 @@ const sequelize = new Sequelize(
     host: process.env.MYSQL_DB_HOST,
     dialect: 'mysql',
     port: parseInt(process.env.MYSQL_DB_PORT || '3306'),
-    logging: false,
+    logging: false, 
   }
 );
+
+// Optional: log only successful or failed connection
+sequelize.authenticate()
+  .then(() => {
+    logger.info('✅ Database connection has been established successfully.');
+  })
+  .catch((error) => {
+    logger.error('❌ Unable to connect to the database:', error);
+  });
 
 export default sequelize;
